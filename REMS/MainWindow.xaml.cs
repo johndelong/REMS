eef6dd949865508da3b664ace75aaa25d7e21246
John DeLong <johnnydelong@gmail.com>
2014-04-30 16:39:39 -0400

    Added Image Crop/Zoom feature

--- REMS/MainWindow.xaml.cs
+++ REMS/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace REMS
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+
         AnalogWaveform<double> analogWaveform = new AnalogWaveform<double>(0);
         string mFileName;
 
@@ -29,7 +31,15 @@ namespace REMS
         {
             InitializeComponent();
             analyzeGrid.ItemsSource = LoadCollectionData();
+            setupHeatMap(50, 50);
         }
+
+        // TODO: Show current x,y,z location on main panel
+        // TODO: Implement selection area
+        // TODO: Add "Take Picture" button somewhere
+
+        // select image to set scan area and zoom in on picture
+        //http://www.codeproject.com/Articles/148503/Simple-Drag-Selection-in-WPF
 
         private List<AnalyzeSynopsisData> LoadCollectionData()
         {
@@ -54,10 +64,9 @@ namespace REMS
                 state = "passed"
             });
 
-           
-            return authors;
 
-        } 
+            return authors;
+        }
 
         // saving user settings (preferences)
         // http://stackoverflow.com/questions/3784477/c-sharp-approach-for-saving-user-settings-in-a-wpf-application
@@ -79,28 +88,78 @@ namespace REMS
 
             graph1.DataSource = analogWaveform;
 
-            //for (int i = 0; i < numberOfPoints; i++)
-            //{
-                //draw_heatMap(100);
-            //}
+            // generate intensity graph
+            // http://zone.ni.com/reference/en-XX/help/372636F-01/mstudiowebhelp/html/wpfintensitygraphplotting/
 
-            /*Rectangle pixel = new Rectangle();
-            pixel.Width = 100;
-            pixel.Height = 100;
 
-            Color temp = new Color();
-            temp = Colors.Red;
+            //intensityGraph1.DataSource = GetData(10);
 
-            SolidColorBrush pixelFill = new SolidColorBrush(temp);
 
-            pixel.Fill = pixelFill;
-            pixel.Opacity = 0.8;
+            colorHeatMap(50, 50);
+        }
 
-            Canvas.SetLeft(pixel, 0);
-            Canvas.SetTop(pixel, 0);
-            image_canvas.Children.Add(pixel);*/
+        //http://www.c-sharpcorner.com/uploadfile/mahesh/grid-in-wpf/
+        private void setupHeatMap(int x, int y)
+        {
+            RowDefinition rowDef;
+            ColumnDefinition colDef;
+
+            for (int lRow = 0; lRow < x; lRow++)
+            {
+                rowDef = new RowDefinition();
+                heat_map.RowDefinitions.Insert(heat_map.RowDefinitions.Count, rowDef);
+                Console.WriteLine(heat_map.RowDefinitions.IndexOf(rowDef).ToString());
+            }
+
+            for (int lCol = 0; lCol < y; lCol++)
+            {
+                colDef = new ColumnDefinition();
+                heat_map.ColumnDefinitions.Insert(heat_map.ColumnDefinitions.Count, colDef);
+                Console.WriteLine(heat_map.ColumnDefinitions.IndexOf(colDef).ToString());
+            }
 
         }
+
+        private void colorHeatMap(int x, int y)
+        {
+            heat_map.Children.Clear();
+
+            Random random = new Random();
+            for (int lRow = 0; lRow < x; lRow++)
+            {
+                for (int lCol = 0; lCol < y; lCol++)
+                {
+
+                    Rectangle pixel = new Rectangle();
+                    Color temp = new Color();
+
+                    int sel = random.Next(10) % 3;
+
+                    switch (sel)
+                    {
+                        case 0:
+                            temp = Colors.Red;
+                            break;
+                        case 1:
+                            temp = Colors.Blue;
+                            break;
+                        case 2:
+                            temp = Colors.Yellow;
+                            break;
+                    }
+
+                    SolidColorBrush pixelFill = new SolidColorBrush(temp);
+                    pixel.Fill = pixelFill;
+                    pixel.Opacity = 0.8;
+
+                    Grid.SetColumn(pixel, lCol);
+                    Grid.SetRow(pixel, lRow);
+
+                    heat_map.Children.Add(pixel);
+                }
+            }
+        }
+
 
         private void open_file(object sender, RoutedEventArgs e)
         {
@@ -123,7 +182,7 @@ namespace REMS
                 mFileName = dlg.FileName;
                 //textBox1.Text = mFilename;
 
-                String [] tokens = mFileName.Split('.');
+                String[] tokens = mFileName.Split('.');
 
                 if (tokens[1] == "csv")
                 {
@@ -171,30 +230,182 @@ namespace REMS
 
         private void open_image()
         {
-            /*Rectangle exampleRectangle = new Rectangle();
-            exampleRectangle.Width = 300;
-            exampleRectangle.Height = 300;
-
-            // Create an ImageBrush and use it to 
-            // paint the rectangle.
-            ImageBrush myBrush = new ImageBrush();
-            //myBrush.ImageSource = new BitmapImage(new Uri(@"C:\Users\delongja\Documents\Visual Studio 2013\Projects\ReadCSV\Audi.jpg", UriKind.Relative));
-            myBrush.ImageSource = new BitmapImage(new Uri(mFileName, UriKind.Relative));
-
-            exampleRectangle.Fill = myBrush;
-
-            Canvas.SetRight(exampleRectangle, 0);
-            Canvas.SetTop(exampleRectangle, 0);
-
-            image_canvas.Children.Add(exampleRectangle);*/
-
-            ImageBrush brush = new ImageBrush();
+            /*ImageBrush brush = new ImageBrush();
             brush.ImageSource = new BitmapImage(new Uri(mFileName, UriKind.Relative));
-            image_canvas.Background = brush;
+            image_canvas.Background = brush;*/
+
+            pcb_image.Source = new BitmapImage(new Uri(mFileName));
         }
+
+        //
+        //
+        // Drag area
+        //
+        //
+        bool mouseDown = false; // Set to 'true' when mouse is held down.
+        Point gridMouseDownPos; // The point where the mouse button was clicked down on the grid.
+        Point imageMouseDownPos; // The point where the mouse button was clicked down on the image.
+        Point gridMouseUpPos;
+        Point imageMouseUpPos;
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Capture and track the mouse.
+            mouseDown = true;
+            gridMouseDownPos = e.GetPosition(theGrid);
+            imageMouseDownPos = e.GetPosition(pcb_image);
+
+            //theGrid.CaptureMouse();
+
+            // Initial placement of the drag selection box.         
+            Canvas.SetLeft(selectionBox, gridMouseDownPos.X);
+            Canvas.SetTop(selectionBox, gridMouseDownPos.Y);
+            selectionBox.Width = 0;
+            selectionBox.Height = 0;
+
+            // Make the drag selection box visible.
+            selectionBox.Visibility = Visibility.Visible;
+
+            //Console.WriteLine("\nX: " + mouseDownPos.X + "\nY: " + mouseDownPos.Y);
+            //Console.WriteLine("\nImageX: " + point.X + "\nImageY: " + point.Y);
+        }
+
+        private void Grid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            // Release the mouse capture and stop tracking it.
+            mouseDown = false;
+            //theGrid.ReleaseMouseCapture();
+
+            // Hide the drag selection box.
+            //selectionBox.Visibility = Visibility.Collapsed;
+
+            gridMouseUpPos = e.GetPosition(theGrid);
+            imageMouseUpPos = e.GetPosition(pcb_image);
+
+            // TODO: 
+            //
+            // The mouse has been released, check to see if any of the items 
+            // in the other canvas are contained within mouseDownPos and 
+            // mouseUpPos, for any that are, select them!
+            //
+        }
+
+        public void set_area_click(object sender, RoutedEventArgs e)
+        {
+            Point topLeft = new Point();
+            Point bottomRight = new Point();
+
+            if (imageMouseDownPos.X < imageMouseUpPos.X && imageMouseDownPos.Y < imageMouseUpPos.Y) // top left to bottom right
+            {
+                topLeft = imageMouseDownPos;
+                bottomRight = imageMouseUpPos;
+            }
+            else if(imageMouseDownPos.X > imageMouseUpPos.X && imageMouseDownPos.Y > imageMouseUpPos.Y) // bottom right to top left
+            {
+                topLeft = imageMouseUpPos;
+                bottomRight = imageMouseDownPos;
+            }
+            else if (imageMouseDownPos.X > imageMouseUpPos.X && imageMouseDownPos.Y < imageMouseUpPos.Y) // top right to bottom left
+            {
+                topLeft.X = imageMouseUpPos.X;
+                topLeft.Y = imageMouseDownPos.Y;
+                bottomRight.X = imageMouseDownPos.X;
+                bottomRight.Y = imageMouseUpPos.Y;
+            }
+            else if (imageMouseDownPos.X < imageMouseUpPos.X && imageMouseDownPos.Y > imageMouseUpPos.Y) // bottom left to top right
+            {
+                topLeft.X = imageMouseDownPos.X;
+                topLeft.Y = imageMouseUpPos.Y;
+                bottomRight.X = imageMouseUpPos.X;
+                bottomRight.Y = imageMouseDownPos.Y;
+            }
+
+            // Convert selected coordinates to actual image coordinates
+            Double Xbegin = (topLeft.X * pcb_image.Source.Width) / pcb_image.ActualWidth;
+            Double Ybegin = (topLeft.Y * pcb_image.Source.Height) / pcb_image.ActualHeight;
+            Double Xend = (bottomRight.X * pcb_image.Source.Width) / pcb_image.ActualWidth;
+            Double Yend = (bottomRight.Y * pcb_image.Source.Height) / pcb_image.ActualHeight;
+            
+            // Convert coordinates integers
+            int xPos = Convert.ToInt32(Math.Round(Xbegin, 0, MidpointRounding.ToEven));
+            int yPos = Convert.ToInt32(Math.Round(Ybegin, 0, MidpointRounding.ToEven));
+            int width = Convert.ToInt32(Math.Round(Xend - Xbegin, 0, MidpointRounding.ToEven));
+            int height = Convert.ToInt32(Math.Round(Yend - Ybegin, 0, MidpointRounding.ToEven));
+            
+            // Create the cropped image
+            var fullImage = pcb_image.Source;
+            var croppedImage = new CroppedBitmap((BitmapSource)fullImage, new Int32Rect(xPos, yPos, width, height));
+            pcb_image.Source = croppedImage;
+
+            // Clear the selection (if there is one)
+            selectionBox.Visibility = Visibility.Collapsed;
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                // When the mouse is held down, reposition the drag selection box.
+
+                Point mouseImagePos = e.GetPosition(pcb_image);
+                Point mousePos = e.GetPosition(theGrid);
+
+                // Restrict movement to image of PCB
+                if (mouseImagePos.X < 0) mousePos.X = mousePos.X - mouseImagePos.X;
+                if (mouseImagePos.X > pcb_image.ActualWidth) mousePos.X = mousePos.X - (mouseImagePos.X - pcb_image.ActualWidth);
+                if (mouseImagePos.Y < 0) mousePos.Y = mousePos.Y - mouseImagePos.Y;
+                if (mouseImagePos.Y > pcb_image.ActualHeight) mousePos.Y = mousePos.Y - (mouseImagePos.Y - pcb_image.ActualHeight);
+
+                // Position box
+                if (gridMouseDownPos.X < mousePos.X)
+                {
+                    Canvas.SetLeft(selectionBox, gridMouseDownPos.X);
+                    selectionBox.Width = mousePos.X - gridMouseDownPos.X;
+                }
+                else
+                {
+                    Canvas.SetLeft(selectionBox, mousePos.X);
+                    selectionBox.Width = gridMouseDownPos.X - mousePos.X;
+                }
+
+                if (gridMouseDownPos.Y < mousePos.Y)
+                {
+                    Canvas.SetTop(selectionBox, gridMouseDownPos.Y);
+                    selectionBox.Height = mousePos.Y - gridMouseDownPos.Y;
+                }
+                else
+                {
+                    Canvas.SetTop(selectionBox, mousePos.Y);
+                    selectionBox.Height = gridMouseDownPos.Y - mousePos.Y;
+                }
+            }
+        }
+
+        /*
+         * Obsolete Code
+         * 
+        
+        Rectangle exampleRectangle = new Rectangle();
+        exampleRectangle.Width = 300;
+        exampleRectangle.Height = 300;
+
+        // Create an ImageBrush and use it to 
+        // paint the rectangle.
+        ImageBrush myBrush = new ImageBrush();
+        //myBrush.ImageSource = new BitmapImage(new Uri(@"C:\Users\delongja\Documents\Visual Studio 2013\Projects\ReadCSV\Audi.jpg", UriKind.Relative));
+        myBrush.ImageSource = new BitmapImage(new Uri(mFileName, UriKind.Relative));
+
+        exampleRectangle.Fill = myBrush;
+
+        Canvas.SetRight(exampleRectangle, 0);
+        Canvas.SetTop(exampleRectangle, 0);
+
+        image_canvas.Children.Add(exampleRectangle);
 
         private void draw_heatMap(double size)
         {
+          
+         
             Random random = new Random();
             int x_pos = 0;
             int y_pos = 0;
@@ -242,5 +453,8 @@ namespace REMS
                 y_pos = y_pos + y_step;
             }
         }
+         * */
+
+
     }
 }
